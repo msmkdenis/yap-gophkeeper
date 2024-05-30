ba25b22173b4dfa5d4865c71ac6d61bab9cf58c2
Denis Burtsev <burcev.denis3@wb.ru>
2024-05-30 10:39:04 +0300

    tests: crypt service

--- internal/encryption/crypt_service_test.go
+++ internal/encryption/crypt_service_test.go
@@ -1,10 +1,11 @@
 package encryption
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
-	"testing"
 )
 
 type CryptServiceTestSuite struct {
